c7f0b04aebb4642e827506f4cbd8e0b9d95ce4cc
Mario Andres Calle <marioandrescallezapata@gmail.com>
2026-02-12 21:07:05 -0500

    SQL

--- create_db.sql/db.sql
+++ create_db.sql/db.sql
@@ -23,3 +23,24 @@ modify column nombre varchar(150);
 alter table cursos 
 modify column nombre varchar(150);
 
+/*Base de datos de una Tienda*/
+
+create table clientes(
+id int primary key auto_increment,
+nombre varchar(100) not null,
+correo varchar(150) unique not null,
+telefono varchar(20)
+);
+
+create table producto(
+id int primary key auto_increment,
+nombre varchar(100) not null,
+precio decimal(10,2) not null,
+stock int not null default 0
+);
+
+create table ventas(
+id int primary key auto_increment,
+fecha date not null,
+total decimal(10,2) not null 
+);
